2bdb9d7855a24606323490999fd1c062f674ee32
Guilherme Amancio <guilhermepamancio@live.com>
2025-04-22 11:05:08 -0300

    Implements create method in service

---
+++ src/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class UserService {
+
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createUserDto: CreateUserDto) {
+
+    //TODO: encrypt password
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 1)
+    }
+
+    //persiste o user criado no banco de dados
+    const createdUser = await this.prisma.user.create({ data })
+
+
+    return {
+      ...createdUser,
+      password: undefined
+    };
+  }
+
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+}
