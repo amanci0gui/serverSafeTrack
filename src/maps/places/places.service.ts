0936038b1b31bd4d537cf218752523a1d413cff3
Guilherme Amancio <guilhermepamancio@live.com>
2025-04-29 18:06:29 -0300

    feat: adds the logic to get the info of the location from google places api

--- src/maps/places/places.service.ts
+++ src/maps/places/places.service.ts
@@ -1,8 +1,24 @@
+import { Client as GoogleMapsClient, PlaceInputType } from '@googlemaps/google-maps-services-js';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class PlacesService {
-    findPlace(text: string) {
-        throw new Error('Method not implemented.');
+export class PlacesService {    
+    constructor(
+        private configService: ConfigService,
+        private googleMapsCliente: GoogleMapsClient,
+    ) {}
+
+
+    async findPlace(text: string) {
+        const {data} = await this.googleMapsCliente.findPlaceFromText({
+            params: {
+                input: text,
+                inputtype: PlaceInputType.textQuery,
+                fields: ['formatted_adress', 'geometry', 'name', 'place_id '],
+                key: this.configService.get('GOOGLE_MAPS_API_KEY')!,
+            },
+        });
+        return data;
     }
 }
