dde238af18396971757fc215cbe079ffb2915623
Guilherme Amancio <guilhermepamancio@live.com>
2025-05-02 23:00:33 -0300

    fix: creating provider in maps module to reference google maps client for dependency injection

--- src/maps/places/places.controller.ts
+++ src/maps/places/places.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PlacesService } from './places.service';
+import { Roles } from 'src/auth/decorators/roles.decorators';
 
 @Controller('places')
 export class PlacesController {
     constructor(private readonly placesService: PlacesService) {}
     // Define your endpoints here
 
+    @Roles("USER")
+    @Roles("ADMIN")
     @Get()
     findPlace(@Query("text") text: string) {
         return this.placesService.findPlace(text);
