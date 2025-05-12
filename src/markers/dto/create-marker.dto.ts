5845e2735f4c171bcb1faf778708674d232388ea
Guilherme Amancio <guilhermepamancio@live.com>
2025-05-12 11:29:01 -0300

    feat: creating dto to create a marker

--- src/markers/dto/create-marker.dto.ts
+++ src/markers/dto/create-marker.dto.ts
@@ -1,1 +1,29 @@
-export class CreateMarkerDto {}
+import { CrimeType } from "@prisma/client";
+import { IsDate, IsEnum, IsNumber, IsString, MaxLength } from "class-validator";
+
+
+export class CreateMarkerDto {
+    @IsString()
+    title: string;
+
+    @IsString()
+    @MaxLength(250)
+    description: string;
+    
+    @IsEnum(CrimeType)
+    category: CrimeType;
+
+    @IsDate()
+    date: Date;
+
+    time: Date;
+
+    @IsNumber()
+    latitude: number;
+
+    @IsNumber()
+    longitude: number;
+
+    @IsNumber()
+    userId: number;
+}
