eff29cc032aa6973c937e0f37b6ac9e5e0ab9a3c
Guilherme Amancio <guilhermepamancio@live.com>
2025-05-12 10:07:19 -0300

    feat: creates user entity

--- src/markers/entities/marker.entity.ts
+++ src/markers/entities/marker.entity.ts
@@ -1,1 +1,16 @@
-export class Marker {}
+import { CrimeType } from "@prisma/client";
+import { User } from "src/user/entities/user.entity";
+export class Marker {
+    id: string;
+    title: string;
+    description: string;
+    category: CrimeType;
+    date: Date;
+    time: Date;
+    latitude: number;
+    longitude: number;
+    createdAt: Date;
+    active: boolean;
+    user: User;
+} 
+
