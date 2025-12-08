7bf36acc17a1c696d4d23b6eaa574af86ce651b2
Guilherme Amancio <guilhermepamancio@live.com>
2025-12-08 10:50:48 -0300

    fix(dto): add validation decorators for email and password fields in ChangePasswordDto

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,10 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class ChangePasswordDto {
   @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   oldPassword: string;
+
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   newPassword: string;
 }
